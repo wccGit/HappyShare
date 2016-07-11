4ece31895e270a1d389603ab7737b6f77d506337
wccGit <yhzhan1002>
2016-07-11 22:14:16 +0800

    优化一 2016/7/11

--- app/src/main/java/com/example/zyh/myapplication/fragment/UserFragment.java
+++ app/src/main/java/com/example/zyh/myapplication/fragment/UserFragment.java
@@ -14,10 +14,8 @@ import android.widget.SimpleAdapter;
 import android.widget.Toast;
 
 import com.example.zyh.myapplication.AboutTen;
-import com.example.zyh.myapplication.MainActivity;
 import com.example.zyh.myapplication.R;
 import com.example.zyh.myapplication.Sygesst;
-
 
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
@@ -30,18 +28,18 @@ import java.util.Map;
 
 public class UserFragment extends Fragment {
     private List<Map<String, Object>> ds;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
-       String [] name ={"我的收藏","字体设置","关于十个","意见反馈"};
-        int [] picture ={R.mipmap.setting_favorite,R.mipmap.setting_aboutus,R.mipmap.setting_feedback,R.mipmap.setting_font};
+        String[] name = {"我的收藏", "字体设置", "关于十个", "意见反馈"};
+        int[] picture = {R.mipmap.setting_favorite, R.mipmap.setting_aboutus, R.mipmap.setting_feedback, R.mipmap.setting_font};
         super.onCreate(savedInstanceState);
         ds = new LinkedList<>();
-        for (int i=0;i<4;i++){
-           Map<String, Object> map =new LinkedHashMap<>();
-            map.put("tv",name[i]);
-map.put("iv",picture[i]);
+        for (int i = 0; i < 4; i++) {
+            Map<String, Object> map = new LinkedHashMap<>();
+            map.put("tv", name[i]);
+            map.put("iv", picture[i]);
             ds.add(map);
-
 
 
         }
@@ -53,27 +51,27 @@ map.put("iv",picture[i]);
         ListView lv = (ListView) view.findViewById(R.id.id_lv_user);
         ImageView iv = (ImageView) view.findViewById(R.id.id_login_user);
         SimpleAdapter adapter = new SimpleAdapter(getActivity(), ds, R.layout.item_user,
-                new String[] { "tv","iv" },
-                new int[] { R.id.tv_id, R.id.iv_id });
+                new String[]{"tv", "iv"},
+                new int[]{R.id.tv_id, R.id.iv_id});
         lv.setAdapter(adapter);
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 //Toast.makeText(getActivity(),"我被点了",Toast.LENGTH_LONG).show();
-                switch(i){
+                switch (i) {
                     case 0:
-                        Toast.makeText(getActivity(),"我被点了",Toast.LENGTH_LONG).show();
+                        Toast.makeText(getActivity(), "我被点了", Toast.LENGTH_LONG).show();
                         break;
                     case 1:
-                        Toast.makeText(getActivity(),"我被点了,呵呵",Toast.LENGTH_LONG).show();
+                        Toast.makeText(getActivity(), "我被点了,呵呵", Toast.LENGTH_LONG).show();
 
                         break;
                     case 2:
-                        Intent intent =new Intent(getActivity(),AboutTen.class);
+                        Intent intent = new Intent(getActivity(), AboutTen.class);
                         startActivity(intent);
                         break;
                     case 3:
-                        Intent intent2 =new Intent(getActivity(),Sygesst.class);
+                        Intent intent2 = new Intent(getActivity(), Sygesst.class);
                         startActivity(intent2);
                         break;
                 }
@@ -83,7 +81,7 @@ map.put("iv",picture[i]);
         iv.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
-                Toast.makeText(getActivity(),"我被点了",Toast.LENGTH_LONG).show();
+                Toast.makeText(getActivity(), "我被点了", Toast.LENGTH_LONG).show();
                 return false;
             }
         });
